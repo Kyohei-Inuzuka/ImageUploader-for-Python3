c0402a605ccd2448aa91665883a1d8169777366c
Kyohei Inuzuka <kyohei.inuzuka@ad.anthill.jp>
2019-10-18 12:06:44 +0900

    Update for Python3

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ImageUploader-for-Python3
  画像ファイルからサムネイルを作成し、Amazon S3へアップロードを行う Python バッチ
+
+## Description
+ Create thumbnails from image files and upload to Amazon S3.
+
+## Requirement
+Python 3
+
+## Install
+```
+cd src
+pip install -r requirements.txt
+```
+
+## Licence
+
+[MIT](https://github.com/tcnksm/tool/blob/master/LICENCE)
---
+++ src/bootstrap.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+CONFIG = {
+    'S3_ROOT': './s3/',
+    'IMAGE_FILE_ROOT': './images',
+    'THUMBNAIL_FILE_ROOT': './s3/images/thumbnail/',
+    'THUMBNAIL_PREFIX': '_thumb',
+    'THUMBNAIL_WIDTH': 300,
+
+    'AWS_ACCESS_KEY_ID': 'Input AWS secret access key.',
+    'AWS_SECRET_ACCESS_KEY': 'Input AWS secret access key.',
+    'S3_REGION': 'Input region.',
+    'S3_BUCKETNAME': 'Input S3 bucket name.'
+}
